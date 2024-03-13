a755978e20e805f9a10d96fb9d5de506690a8dff
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-03-13 17:29:17 +0100

    update

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
-// // 'use strict';
+// 'use strict';
 
-// const btn = document.querySelector('.btn-country');
-// const countriesContainer = document.querySelector('.countries');
-// const renderError = function(msg){
-//     countriesContainer.insertAdjacentText('beforeend', msg);
-//     countriesContainer.style.opacity =1; 
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+const renderError = function(msg){
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    countriesContainer.style.opacity =1; 
 
-// }; 
+}; 
 
-// const renderCountry = function(data, className = ''){
-//   // Check if data is an array (for neighbor countries)
-//   if (Array.isArray(data)) {
-//       data.forEach(neighbor => {
-//           // Extract languages if available
-//           const languages = neighbor.languages ? Object.values(neighbor.languages).join(', ') : 'N/A';
-//           // Extract currencies if available
-//           const currencies = neighbor.currencies ? Object.keys(neighbor.currencies).map(currencyCode => {
-//               const currency = neighbor.currencies[currencyCode];
-//               return `${currency.name} (${currency.symbol})`;
-//           }).join(', ') : 'N/A';
+const renderCountry = function(data, className = ''){
+  // Check if data is an array (for neighbor countries)
+  if (Array.isArray(data)) {
+      data.forEach(neighbor => {
+          // Extract languages if available
+          const languages = neighbor.languages ? Object.values(neighbor.languages).join(', ') : 'N/A';
+          // Extract currencies if available
+          const currencies = neighbor.currencies ? Object.keys(neighbor.currencies).map(currencyCode => {
+              const currency = neighbor.currencies[currencyCode];
+              return `${currency.name} (${currency.symbol})`;
+          }).join(', ') : 'N/A';
 
-//           const html  = `
-//           <article class="country ${className}">
-//               <img class="country__img" src="${neighbor.flags.png ? neighbor.flags.png : 'N/A'}" />
-//               <div class="country__data">
-//                   <h3 class="country__name">${neighbor.name.common}</h3>
-//                   <h4 class="country__region">${neighbor.region}</h4>
-//                   <p class="country__row"><span>👫</span>${(+neighbor.population/1000000).toFixed(1)} people</p>
-//                   <p class="country__row"><span>🗣️</span>${languages}</p>
-//                   <p class="country__row"><span>💰</span>${currencies}</p>
-//               </div>
-//           </article>`;
+          const html  = `
+          <article class="country ${className}">
+              <img class="country__img" src="${neighbor.flags.png ? neighbor.flags.png : 'N/A'}" />
+              <div class="country__data">
+                  <h3 class="country__name">${neighbor.name.common}</h3>
+                  <h4 class="country__region">${neighbor.region}</h4>
+                  <p class="country__row"><span>👫</span>${(+neighbor.population/1000000).toFixed(1)} people</p>
+                  <p class="country__row"><span>🗣️</span>${languages}</p>
+                  <p class="country__row"><span>💰</span>${currencies}</p>
+              </div>
+          </article>`;
 
-//           countriesContainer.insertAdjacentHTML('beforeend', html); 
-//           countriesContainer.style.opacity = 1;
-//       });
-//   } else {
-//       // Extract languages if available
-//       const languages = data.languages ? Object.values(data.languages).join(', ') : 'N/A';
-//       // Extract currencies if available
-//       const currencies = data.currencies ? Object.keys(data.currencies).map(currencyCode => {
-//           const currency = data.currencies[currencyCode];
-//           return `${currency.name} (${currency.symbol})`;
-//       }).join(', ') : 'N/A';
+          countriesContainer.insertAdjacentHTML('beforeend', html); 
+          countriesContainer.style.opacity = 1;
+      });
+  } else {
+      // Extract languages if available
+      const languages = data.languages ? Object.values(data.languages).join(', ') : 'N/A';
+      // Extract currencies if available
+      const currencies = data.currencies ? Object.keys(data.currencies).map(currencyCode => {
+          const currency = data.currencies[currencyCode];
+          return `${currency.name} (${currency.symbol})`;
+      }).join(', ') : 'N/A';
 
-//       const html  = `
-//       <article class="country ${className}">
-//           <img class="country__img" src="${data.flags.png ? data.flags.png : 'N/A'}" />
-//           <div class="country__data">
-//               <h3 class="country__name">${data.name.common}</h3>
-//               <h4 class="country__region">${data.region}</h4>
-//               <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
-//               <p class="country__row"><span>🗣️</span>${languages}</p>
-//               <p class="country__row"><span>💰</span>${currencies}</p>
-//           </div>
-//       </article>`;
+      const html  = `
+      <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png ? data.flags.png : 'N/A'}" />
+          <div class="country__data">
+              <h3 class="country__name">${data.name.common}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${languages}</p>
+              <p class="country__row"><span>💰</span>${currencies}</p>
+          </div>
+      </article>`;
 
-//       countriesContainer.insertAdjacentHTML('beforeend', html); 
-//       countriesContainer.style.opacity = 1;
-//   }
-// }
+      countriesContainer.insertAdjacentHTML('beforeend', html); 
+      countriesContainer.style.opacity = 1;
+  }
+}
 
 // // // ////////////////////////////////////////
 
@@ -231,18 +231,18 @@
 // Create a Simple promise
 
 
-const loteryPromise = new Promise(function(resolve, reject){
-console.log('Lotery draw is happening 🪄 ');
-setTimeout(function() {
-    if(Math.random() >= 0.5){ // to set the Promise as fullfilled we call the resolved function 
-        resolve('You win!💥')
-    }else{
-        reject(new Error ('You lost 🥲'));
-    }
-}, 2000)
-});
+// const loteryPromise = new Promise(function(resolve, reject){
+// console.log('Lotery draw is happening 🪄 ');
+// setTimeout(function() {
+//     if(Math.random() >= 0.5){ // to set the Promise as fullfilled we call the resolved function 
+//         resolve('You win!💥')
+//     }else{
+//         reject(new Error ('You lost 🥲'));
+//     }
+// }, 2000)
+// });
 
-loteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+// loteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
 
 
@@ -254,9 +254,94 @@ return new Promise(function(resolve){
 });
 };
 
-wait(2).then(() => {
-    console.log('I waited for 2 seconds');
-    return wait(1); 
-}).then(() => {
-    console.log("I waited for 1 second");
-});
+// wait(2).then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1); 
+// }).then(() => {
+//     console.log("I waited for 1 second");
+// });
+
+
+//Promisify the Geolocation API 
+
+const getPosition =  function (){
+ return new Promise(function(resolve, reject){
+    // navigator.geolocation.getCurrentPosition(position => resolve(position), err => reject(err)); 
+    navigator.geolocation.getCurrentPosition(resolve, reject); 
+ });
+};
+// getPosition().then(pos => console.log(pos)); 
+
+const whereAmI = function () {
+  getPosition().then(pos=> {
+    const {latitude:lat, longitude: lng} = pos.coords;
+    return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`) 
+  })
+
+          .then(res => {
+            if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+            return res.json();
+          })
+          .then(data => {
+            console.log(data);
+            console.log(`You are in ${data.city}, ${data.country}`);
+      
+            return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+          })
+          .then(res => {
+            if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      
+            return res.json();
+          })
+          .then(data => renderCountry(data[0]))
+          .catch(err => console.error(`${err.message} 💥`));
+      };
+
+    
+    btn.addEventListener('click', whereAmI); 
+
+
+    const imgContainer = document.querySelector('.images');
+
+    const createImage = function(imgPath){
+        return new Promise(function(resolve, reject){
+           const img =  document.createElement('img'); 
+           img.src = imgPath; 
+           console.log(imgPath);
+
+           img.addEventListener('load', function(){
+             imgContainer.append(img); 
+             resolve(img); 
+           }); 
+
+           img.addEventListener('error', function(){
+            reject (new Error('Image not found')); 
+           });
+        });
+    };
+
+
+    let currentImg; 
+
+    createImage('img/img-1.jpg')
+    .then(img => {
+        currentImg= img;
+        console.log('Image 1 loaded');
+        return wait(2)
+    })
+    .then(()=> {
+        currentImg.style.display = 'none';
+        return(createImage('img/img-2.jpg').then(img => {
+            console.log('Image 2 loaded');
+            return wait(2); 
+        }))
+    })
+    .then(img => {
+       currentImg= img;
+        return(createImage('img/img-3.jpg').then(img => {
+            console.log('Image 3 loaded');
+            return wait(2); 
+        }))
+    })
+    .then(()=> currentImg.style.display = 'none')
+    .catch(err => console.log(err)); 
